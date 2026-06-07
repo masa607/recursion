2b5b37d9f351746c23817acd0eeca13f2b4604e9
masa607 <jizomasa.06.07@gmail.com>
2026-06-07 23:19:54 +0900

    コマンドにnameとうつと人の名前が返されるようにしました

---
+++ Local_Chat_Messenger/assignment/server.py
@@ -0,0 +1,31 @@
+from faker import Faker
+
+import socket
+import os
+sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+server_address = '/tmp/socket_file'
+try:
+    os.unlink(server_address)
+except FileNotFoundError:
+    pass
+print('Starting up on {}'.format(server_address))
+sock.bind(server_address)
+sock.listen(1)
+while True:
+    connection, client_address = sock.accept()
+    try:
+        print('connection from', client_address)
+        while True:
+            data = connection.recv(16)
+            data_str =  data.decode('utf-8')
+            print('Received ' + data_str)
+            if not data:
+                print('no data from', client_address)
+                break
+            elif data_str == "name":
+                fake = Faker()
+                response = 'Processing ' + fake.name()
+                connection.sendall(response.encode())
+    finally:
+        print("Closing current connection")
+        connection.close()
